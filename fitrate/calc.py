71efa2e3d10a079beaba1fd82a188080d3a4aef7
suzukey <suzukei0208@gmail.com>
2020-09-10 17:37:53 +0900

    🎨 Add typehint

--- fitrate/calc.py
+++ fitrate/calc.py
@@ -1,21 +1,27 @@
 import math
 import operator
 from functools import reduce
+from typing import List, Tuple, Union
 
 
-def calc(sizes, max_volume, dimension=2, is_int=True):
+def calc(
+    sizes: Union[List[int], Tuple[int, ...]],
+    max_volume: int,
+    is_int: bool = True,
+) -> Tuple[float, ...]:
     """
     Calculate the length of the sides that fit into the maximum volume
     """
-    divisor = gcd(*sizes)
+    dimension: int = len(sizes)
+    divisor: int = gcd(*sizes)
 
-    weights = [(size / divisor) for size in sizes]
-    total_weights = prod(*weights)
+    weights: List[float] = [(size / divisor) for size in sizes]
+    total_weights: float = prod(*weights)
 
-    x_nth_power = max_volume / total_weights
-    x_nth_root = math.pow(x_nth_power, 1 / dimension)
+    x_nth_power: float = max_volume / total_weights
+    x_nth_root: float = math.pow(x_nth_power, 1 / dimension)
 
-    sides_length = [
+    sides_length: List[float] = [
         int(weight * x_nth_root) if is_int else (weight * x_nth_root)
         for weight in weights
     ]
@@ -23,14 +29,14 @@ def calc(sizes, max_volume, dimension=2, is_int=True):
     return tuple(sides_length)
 
 
-def prod(*numbers):
+def prod(*numbers: float) -> float:
     """
     Calculate the total product from numbers
     """
     return reduce(operator.mul, numbers)
 
 
-def gcd(*numbers):
+def gcd(*numbers: int) -> int:
     """
     Calculate the greatest common divisor from numbers
     """
