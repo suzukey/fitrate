42616c9b6aebee040bc8b476a4a18f9b0ba791ed
suzukey <suzukei0208@gmail.com>
2020-09-15 07:54:51 +0900

    🔖 Release v1.0.0

--- fitrate/__init__.py
+++ fitrate/__init__.py
@@ -2,7 +2,7 @@
 Calculate the length of the sides that fit into the limited volume
 """
 
-__version__ = "0.2.1"
+__version__ = "1.0.0"
 
 from fitrate import utils
 from fitrate._fit import contain, scale_down
