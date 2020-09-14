364299c395e6d556af0558d0a318c053e8260ead
suzukey <suzukei0208@gmail.com>
2020-09-15 07:19:56 +0900

    ✨ Functions like css object fit

--- fitrate/__init__.py
+++ fitrate/__init__.py
@@ -5,4 +5,4 @@ Calculate the length of the sides that fit into the limited volume
 __version__ = "0.2.1"
 
 from fitrate import utils
-from fitrate.calc import calc
+from fitrate.calc import contain, scale_down
--- fitrate/calc.py
+++ fitrate/calc.py
@@ -3,13 +3,13 @@ from typing import List, Tuple, Union
 from fitrate import utils
 
 
-def calc(
+def contain(
     sizes: Union[List[int], Tuple[int, ...]],
     max_volume: int,
-    is_int: bool = True,
+    resp_is_int: bool = True,
 ) -> Tuple[float, ...]:
     """
-    Calculate the length of the sides that fit into the maximum volume
+    Scale to fit within the maximum volume while maintaining the aspect ratio.
     """
     dimension: int = len(sizes)
     divisor: int = utils.gcd(*sizes)
@@ -21,8 +21,21 @@ def calc(
     x_nth_root: float = utils.nthrt(x_nth_power, dimension)
 
     sides_length: List[float] = [
-        int(weight * x_nth_root) if is_int else (weight * x_nth_root)
+        int(weight * x_nth_root) if resp_is_int else (weight * x_nth_root)
         for weight in weights
     ]
 
     return tuple(sides_length)
+
+
+def scale_down(
+    sizes: Union[List[int], Tuple[int, ...]],
+    max_volume: int,
+    resp_is_int: bool = True,
+) -> Tuple[float, ...]:
+    """
+    Shrink to fit within the maximum volume while maintaining the aspect ratio. Does not expand.
+    """
+    if utils.prod(*sizes) <= max_volume:
+        return tuple(sizes)
+    return contain(sizes, max_volume, resp_is_int)
